605ead59d6f31b2d81c7f07847edc5946d1b17ab
Sergey White <sergey_white86@mail.ru>
2024-03-03 14:24:04 +0300

    hw 10

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Task1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
+
+        System.out.println("Task2");
+        String upperCaseFullName = fullName.toUpperCase();
+        System.out.printf("Данные ФИО сотрудника для заполнения отчета - %s %n",upperCaseFullName);
+
+        System.out.println("Task3");
+        fullName = "Иванов Семён Семёнович";
+        String changeFullName = fullName.replace('ё','е');
+        System.out.printf("Данные ФИО сотрудника - %s",changeFullName);
 
     }
 }
